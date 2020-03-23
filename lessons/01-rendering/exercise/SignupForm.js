c4ded156be9076c69eca16b439bffa02f2103153
Jorge Raúl Díaz Samaniego <jorgediazsamaniego@gmail.com>
2020-03-23 16:15:03 -0400

    Initial commit

---
+++ lessons/01-rendering/exercise/SignupForm.js
@@ -0,0 +1,12 @@
+import React, { Fragment, useState } from "react"
+import VisuallyHidden from "@reach/visually-hidden"
+import { signup } from "app/utils"
+import TabsButton from "app/TabsButton"
+import { FaDumbbell } from "react-icons/fa"
+import { DateFields, MonthField, DayField, YearField } from "app/DateFields"
+// import SignupForm from "./SignupForm.final"
+// export default SignupForm
+
+export default function SignupForm() {
+  return <form className="SignupForm">{/* Your work here */}</form>
+}
