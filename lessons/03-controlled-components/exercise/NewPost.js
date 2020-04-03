a60ee2bf8c755572cad53aededb32d0f4f61b5d1
Jorge Raúl Díaz Samaniego <jorgediazsamaniego@gmail.com>
2020-04-03 16:50:03 -0400

    controlled component

--- lessons/03-controlled-components/exercise/NewPost.js
+++ lessons/03-controlled-components/exercise/NewPost.js
@@ -11,19 +11,32 @@ const MAX_MESSAGE_LENGTH = 200
 
 export default function NewPost({ takeFocus, date, onSuccess, showAvatar }) {
   const [{ auth }] = useAppState()
+  const [message, setMessage] = useState("Ran around the lake.")
+  const messageTooLong = message.length > MAX_MESSAGE_LENGTH
+
+  const handleMessageChange = event => {
+    setMessage(event.target.value)
+  }
 
   return (
-    <div className="NewPost">
+    <div className={"NewPost" + (messageTooLong ? ` ${errorClass}` : "")}>
       {showAvatar && <Avatar uid={auth.uid} size={70} />}
       <form className="NewPost_form">
         <textarea
           className="NewPost_input"
           placeholder="Tell us about your workout!"
+          value={message}
+          onChange={handleMessageChange}
         />
         <div className="NewPost_char_count">
-          0/{MAX_MESSAGE_LENGTH}
+          {message.length}/{MAX_MESSAGE_LENGTH}
         </div>
-        <RecentPostsDropdown uid={auth.uid} onSelect={(message) => {}}/>
+        <RecentPostsDropdown
+          uid={auth.uid}
+          onSelect={message => {
+            setMessage(message)
+          }}
+        />
         <div className="NewPost_buttons">
           <Minutes date={date} />
           <div>
