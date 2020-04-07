d05e68f77dcacfecc95066f2d9464b5608de4efb
Jorge Raúl Díaz Samaniego <jorgediazsamaniego@gmail.com>
2020-04-07 12:36:18 -0400

    composing custom hooks

--- lessons/05-data-loading/exercise/App.js
+++ lessons/05-data-loading/exercise/App.js
@@ -4,9 +4,22 @@ import { onAuthStateChanged } from 'app/utils'
 import LoggedIn from 'app/LoggedIn'
 import LoggedOut from 'app/LoggedOut'
 
+const useAuth = () => {
+  const [authAttempted, setAuthAttempted] = useState(false)
+  const [auth, setAuth] = useState(null)
+
+  useEffect(() => {
+    return onAuthStateChanged(auth => {
+      setAuthAttempted(true)
+      setAuth(auth)
+    })
+  }, [])
+
+  return { auth, authAttempted }
+}
+
 export default function App() {
-  const auth = null
-  const authAttempted = false
+  const { auth, authAttempted } = useAuth()
 
   if (!authAttempted) {
     return <p>Authenticating...</p>
