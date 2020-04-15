2d5e880421804c06138f397981154d7ab1a8ffc0
Jorge Raúl Díaz Samaniego <jorgediazsamaniego@gmail.com>
2020-04-14 21:30:56 -0400

    useReducer, useEffect

--- lessons/10-the-feed/exercise/Feed.js
+++ lessons/10-the-feed/exercise/Feed.js
@@ -1,23 +1,89 @@
-import React from "react"
+import React, { useState, useEffect, useReducer } from "react"
 import FeedPost from "app/FeedPost"
 import { loadFeedPosts, subscribeToNewFeedPosts } from "app/utils"
 // import FeedFinal from './Feed.final'
 // export default FeedFinal
 export default Feed
 
+let feedState = null
+
 function Feed() {
+  const [state, dispatch] = useReducer((state, action) => {
+    switch(action.type) {
+      case 'NEW_POSTS_LOADED': return {
+        ...state,
+        newPosts: action.newPosts
+      }
+      case 'LOAD_POSTS': return {
+        ...state,
+        posts: action.posts
+      }
+      case 'LOAD_NEW_POSTS': return {
+        ...state,
+        time: Date.now(),
+        limit: state.limit + state.newPosts.length
+      }
+      case 'VIEW_MORE': return {
+        ...state,
+        limit: state.limit + 3
+      }
+      default: throw new Error(`Unknown action ${action.type}`)
+    }
+  }, 
+  feedState || {
+    posts: [],
+    time: Date.now(),
+    limit: 3,
+    newPosts: []
+  })
+
+  useEffect(() => {
+    feedState = state
+  })
+
+  const { posts, time, limit, newPosts } = state
+
+  useEffect(() => {
+    return subscribeToNewFeedPosts(time, (newPosts) => {
+      dispatch({ type: 'NEW_POSTS_LOADED', newPosts })
+    })
+  }, [time])
+
+  useEffect(() => {
+    let isCurrent = true
+    loadFeedPosts(time, limit)
+      .then(posts => {
+        if (isCurrent) dispatch({ type: 'LOAD_POSTS', posts })
+      })
+    return () => {
+      isCurrent = false
+    }
+  }, [time, limit])
+
   return (
     <div className="Feed">
-      <div className="Feed_button_wrapper">
-        <button className="Feed_new_posts_button icon_button">
-          View 3 New Posts
-        </button>
-      </div>
+      {newPosts.length > 0 && (
+        <div className="Feed_button_wrapper">
+          <button
+            onClick={() => {
+              dispatch({ type: 'LOAD_NEW_POSTS', newPosts })
+            }}
+          className="Feed_new_posts_button icon_button">
+            View {newPosts.length} New Posts
+          </button>
+        </div>
+      )}
 
-      <FeedPost post={fakePost} />
+      {posts.map((post, index) => (
+        <FeedPost post={post} key={index} />
+      ))}
 
       <div className="Feed_button_wrapper">
-        <button className="Feed_new_posts_button icon_button">View More</button>
+        <button
+          onClick={() => {
+            dispatch({ type: 'VIEW_MORE' })
+          }}
+          className="Feed_new_posts_button icon_button">View More</button>
       </div>
     </div>
   )
